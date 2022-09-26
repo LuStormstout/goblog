7fe465cec492d8a2180bd83730f80a0ff9e645db
Lu Stormstout <jacklu.net@gmail.com>
2022-09-26 17:31:39 +0800

    设置标头

--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import (
 )
 
 func handlerFunc(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "text/html; charset=utf-8")
+
 	if request.URL.Path == "/" {
 		_, err := fmt.Fprint(response, "<h1>Hello, 这里是 GoBlog!</h1>")
 		if err != nil {
 			return
 		}
 	} else if request.URL.Path == "/about" {
-		_, err := fmt.Fprint(response, "此博客是用以记录变成笔记，如您有反馈或建议，请联系 "+
+		_, err := fmt.Fprint(response, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
 			"<a href=\"mailto:lustormstout@gmail.com\">lustormstout@gmail.com</a>")
 		if err != nil {
 			return
