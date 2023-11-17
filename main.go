94aa97f7f01a77d6dd6255ec4bd553d01d3e3ff1
Lu Stormstout <jacklu.net@gmail.com>
2023-11-17 18:50:09 +0800

    重构 RouteName2URL

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"goblog/pkg/route"
 	"html/template"
 	"net/http"
 	"net/url"
@@ -88,17 +89,6 @@ func (a Article) Link() string {
 	return showURL.String()
 }
 
-// RouteName2URL 通过路由名称来获取 URL
-func RouteName2URL(routeName string, pairs ...string) string {
-	url, err := router.Get(routeName).URL(pairs...)
-	if err != nil {
-		checkErr(err)
-		return ""
-	}
-
-	return url.String()
-}
-
 // Int64ToString 将 int64 转换为 string
 func Int64ToString(num int64) string {
 	return strconv.FormatInt(num, 10)
@@ -127,7 +117,7 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 		// 4. 读取成功，显示文章
 		template, err := template.New("show.gohtml").
 			Funcs(template.FuncMap{
-				"RouteName2URL": RouteName2URL,
+				"RouteName2URL": route.RouteName2URL,
 				"Int64ToString": Int64ToString,
 			}).
 			ParseFiles("resources/views/articles/show.gohtml")
---
+++ pkg/route/route.go
@@ -0,0 +1,21 @@
+package route
+
+import "github.com/gorilla/mux"
+
+// Router 路由实例
+var Router *mux.Router
+
+// Initialize 初始化路由实例
+func Initialize() {
+	Router = mux.NewRouter()
+}
+
+// RouteName2URL 根据路由名称获取 URL
+func RouteName2URL(routeName string, pairs ...string) string {
+	url, err := Router.Get(routeName).URL(pairs...)
+	if err != nil {
+		panic(err)
+	}
+
+	return url.String()
+}
