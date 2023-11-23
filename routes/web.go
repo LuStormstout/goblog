ad4bfd60594a16f7f0c28615565da91c029ef921
Lu Stormstout <jacklu.net@gmail.com>
2023-11-24 02:21:06 +0800

    重构中间件

--- routes/web.go
+++ routes/web.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"goblog/app/controllers"
+	"goblog/app/http/middlewares"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -11,11 +12,11 @@ func RegisterWebRoutes(router *mux.Router) {
 	// 静态页面
 	pagesController := new(controllers.PagesController)
 	router.NotFoundHandler = http.HandlerFunc(pagesController.NotFound)
+	router.HandleFunc("/", pagesController.Home).Methods("GET").Name("home")
 	router.HandleFunc("/about", pagesController.About).Methods("GET").Name("about")
 
 	// 文章相关页面
 	articlesController := new(controllers.ArticlesController)
-	router.HandleFunc("/", articlesController.Index).Methods("GET").Name("home")
 	router.HandleFunc("/articles", articlesController.Index).Methods("GET").Name("articles.index")
 	router.HandleFunc("/articles/{id:[0-9]+}", articlesController.Show).Methods("GET").Name("articles.show")
 	router.HandleFunc("/articles/create", articlesController.Create).Methods("GET").Name("articles.create")
@@ -24,12 +25,6 @@ func RegisterWebRoutes(router *mux.Router) {
 	router.HandleFunc("/articles/{id:[0-9]+}", articlesController.Update).Methods("POST").Name("articles.update")
 	router.HandleFunc("/articles/{id:[0-9]+}/delete", articlesController.Delete).Methods("POST").Name("articles.delete")
 
-	// 用户认证
-	authController := new(controllers.AuthController)
-	router.HandleFunc("/auth/register", authController.Register).Methods("GET").Name("auth.register")
-	router.HandleFunc("/auth/do-register", authController.DoRegister).Methods("POST").Name("auth.do-register")
-
-	// 静态资源
-	router.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
-	router.PathPrefix("/js/").Handler(http.FileServer(http.Dir("./public")))
+	// 中间件：强制内容类型为 HTML
+	router.Use(middlewares.ForceHTML)
 }
