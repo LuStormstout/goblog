8c44b56bf0f12fa7b6acb885f4694d697a7c6bce
Lu Stormstout <jacklu.net@gmail.com>
2023-12-07 02:29:09 +0800

    set Home to articles index

--- routes/web.go
+++ routes/web.go
@@ -11,11 +11,11 @@ func RegisterWebRoutes(router *mux.Router) {
 	// 静态页面
 	pagesController := new(controllers.PagesController)
 	router.NotFoundHandler = http.HandlerFunc(pagesController.NotFound)
-	router.HandleFunc("/", pagesController.Home).Methods("GET").Name("home")
 	router.HandleFunc("/about", pagesController.About).Methods("GET").Name("about")
 
 	// 文章相关页面
 	articlesController := new(controllers.ArticlesController)
+	router.HandleFunc("/", articlesController.Index).Methods("GET").Name("home")
 	router.HandleFunc("/articles", articlesController.Index).Methods("GET").Name("articles.index")
 	router.HandleFunc("/articles/{id:[0-9]+}", articlesController.Show).Methods("GET").Name("articles.show")
 	router.HandleFunc("/articles/create", articlesController.Create).Methods("GET").Name("articles.create")
