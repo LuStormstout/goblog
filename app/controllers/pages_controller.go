8e2cee64bab46969aad0eae2493b961e697a6bce
Lu Stormstout <jacklu.net@gmail.com>
2023-12-07 02:17:06 +0800

    Update pages_controller.go

--- app/controllers/pages_controller.go
+++ app/controllers/pages_controller.go
@@ -7,17 +7,17 @@ type PagesController struct {
 }
 
 // Home 首页
-func (*PagesController) Home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("首页"))
+func (*PagesController) Home(w http.ResponseWriter, _ *http.Request) {
+	_, _ = w.Write([]byte("首页"))
 }
 
 // About 关于我们页面
-func (*PagesController) About(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("关于我们"))
+func (*PagesController) About(w http.ResponseWriter, _ *http.Request) {
+	_, _ = w.Write([]byte("关于我们"))
 }
 
 // NotFound 404 页面
-func (*PagesController) NotFound(w http.ResponseWriter, r *http.Request) {
+func (*PagesController) NotFound(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte("404 Not Found"))
+	_, _ = w.Write([]byte("404 Not Found"))
 }
