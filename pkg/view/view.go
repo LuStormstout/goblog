fef9b722693de5feca7b768b4018b68a82ef3a30
Lu Stormstout <jacklu.net@gmail.com>
2023-12-07 02:09:31 +0800

    注册页面

--- pkg/view/view.go
+++ pkg/view/view.go
@@ -9,8 +9,21 @@ import (
 	"strings"
 )
 
-// Render 渲染视图
+// D 是map[string]interface{}类型的简写
+type D map[string]interface{}
+
+// Render 渲染通用视图
 func Render(w io.Writer, data interface{}, tplFiles ...string) {
+	RenderTemplate(w, "app", data, tplFiles...)
+}
+
+// RenderSimple 渲染简单视图
+func RenderSimple(w io.Writer, data interface{}, tplFiles ...string) {
+	RenderTemplate(w, "simple", data, tplFiles...)
+}
+
+// RenderTemplate 渲染视图
+func RenderTemplate(w io.Writer, name string, data interface{}, tplFiles ...string) {
 	// 设置模板相对路径
 	viewDir := "resources/views/"
 
@@ -34,6 +47,6 @@ func Render(w io.Writer, data interface{}, tplFiles ...string) {
 	logger.LogError(err)
 
 	// 渲染模板
-	err = tmpl.ExecuteTemplate(w, "app", data)
+	err = tmpl.ExecuteTemplate(w, name, data)
 	logger.LogError(err)
 }
